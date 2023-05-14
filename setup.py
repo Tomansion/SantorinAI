17ed818e0a2e5a44adcfc6fd1bfd3aabdd29c86a
tom.mansion <tomansion@yahoo.fr>
2023-05-14 15:40:26 +0200

    1.1.1

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="SantorinAI",
+    version="1.1.1",
+    author="Tom Mansion",
+    author_email="tomansion@yahoo.fr",
+    description="A Python library for the Santorini board game",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/tomansion/santorinai",
+    packages=find_packages(),
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+    ],
+    keywords=["santorini", "ai", "boardgame"],
+    python_requires=">=3.6",
+    install_requires=["pysimplegui"],
+)
