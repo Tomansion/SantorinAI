12fb9478732ea0338c84a2dbf1933c119288d4e2
tom.mansion <tomansion@yahoo.fr>
2023-05-14 15:32:28 +0200

    1.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="SantoriAI",
-    version="1.1.0",
+    version="1.1.1",
     author="Tom Mansion",
     author_email="tomansion@yahoo.fr",
     description="A Python library for the Santorini board game",
